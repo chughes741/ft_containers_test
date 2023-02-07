56fb93a46dde2a5bf23ee9724e2afbe24216fd25
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-02-07 17:37:10 -0500

    Refactored stack_tests

--- stack_test.cpp
+++ stack_test.cpp
@@ -7,53 +7,102 @@
 #include <stack>
 #include <vector>
 
-using std::vector;  // Change to ft::vector when it's tested
+using ft::stack;
+using std::vector;  // Change to vector when it's tested
 
-TEST(StackTest, ConstructorAndCopyConstructor) {
-  vector<int> vec;
-
-  ft::stack<int, vector<int>>  ft_stack_1(vec);
-  std::stack<int, vector<int>> std_stack(vec);
-
-  EXPECT_EQ(ft_stack_1.size(), std_stack.size());
-
-  ft::stack<int, vector<int>> ft_stack_2(ft_stack_1);
-  EXPECT_EQ(ft_stack_1.size(), ft_stack_2.size());
+// explicit stack(const Container& cont = Container());
+TEST(StackConstructors, Constructor) {
+  {
+    vector<int> v(10, 10);
+    stack<int>  s(v);
+    EXPECT_EQ(s.size(), 10);
+  }
 }
 
-TEST(StackTest, CopyAssignmentAndOperators) {
-  vector<int> vec_1(10);
-  for (vector<int>::iterator it = vec_1.begin(); it != vec_1.end(); ++it) {
-    *it = 24;
+// stack(const stack& other) : c(other.c);
+TEST(StackConstructors, CopyConstructor) {
+  {
+    vector<int>    v(10, 10);
+    stack<int> s1(v);
+    stack<int> s2(s1);
+    EXPECT_EQ(s1.size(), s2.size());
   }
-  vector<int> vec_2(vec_1);
-  vec_2.push_back(42);
-
-  ft::stack<int, vector<int>> stack_1(vec_1);
-  ft::stack<int, vector<int>> stack_2 = stack_1;
-  ft::stack<int, vector<int>> stack_3(vec_2);
-
-  EXPECT_EQ(stack_1.size(), stack_2.size());
-  EXPECT_TRUE(stack_1 == stack_2);
-  EXPECT_TRUE(stack_1 < stack_3);
 }
 
-TEST(StackTest, TopPushPop) {
-  vector<int> vec_1(10);
-  for (vector<int>::iterator it = vec_1.begin(); it != vec_1.end(); ++it) {
-    *it = rand();
+// stack& operator=(const stack& other);
+TEST(StackAssignment, CopyAssignment) {
+  {  // TODO check for deep/shallow copy
+    vector<int> vec_1(10);
+    for (vector<int>::iterator it = vec_1.begin(); it != vec_1.end(); ++it) {
+      *it = 24;
+    }
+    vector<int> vec_2(vec_1);
+    vec_2.push_back(42);
+
+    stack<int> stack_1(vec_1);
+    stack<int> stack_2 = stack_1;
+    stack<int> stack_3(vec_2);
+
+    EXPECT_EQ(stack_1.size(), stack_2.size());
+    EXPECT_TRUE(stack_1 == stack_2);
+    EXPECT_TRUE(stack_1 < stack_3);
   }
-  vector<int>                  vec_2(vec_1);
-  ft::stack<int, vector<int>>  stack_1(vec_1);
-  std::stack<int, vector<int>> stack_2(vec_2);
+}
 
-  EXPECT_EQ(stack_1.top(), stack_2.top());
+// reference top();
+TEST(StackElementAccess, Top) {
+  {
+    vector<int> v(10, 3);
+    v.push_back(3);
+    stack<int> s(v);
+    EXPECT_EQ(s.top(), v.back());
+  }
+}
 
-  stack_1.pop();
-  stack_2.pop();
-  EXPECT_EQ(stack_1.top(), stack_2.top());
+// bool empty() const;
+TEST(StackCapacity, Empty) {
+  {
+    stack<int> s;
+    EXPECT_TRUE(s.empty());
+  }
+  {
+    vector<int> v(10, 3);
+    v.push_back(3);
+    stack<int> s(v);
+    EXPECT_EQ(s.empty(), v.empty());
+  }
+}
 
-  stack_1.push(42);
-  stack_2.push(42);
-  EXPECT_EQ(stack_1.top(), stack_2.top());
+// size_type size() const;
+TEST(StackCapacity, Size) {
+  {
+    stack<int> s;
+    EXPECT_EQ(s.size(), 0);
+  }
+  {
+    vector<int> v(10, 3);
+    v.push_back(3);
+    stack<int> s(v);
+    EXPECT_EQ(s.size(), v.size());
+  }
+}
+
+// void push(const value_type& value)
+TEST(StackModifiers, Push) {
+  {
+    stack<int> s;
+    s.push(3);
+    EXPECT_EQ(s.top(), 3);
+  }
+}
+
+// void pop()
+TEST(StackModifiers, Pop) {
+  {
+    stack<int> s;
+    s.push(3);
+    s.push(2);
+    s.pop();
+    EXPECT_EQ(s.top(), 3);
+  }
 }
